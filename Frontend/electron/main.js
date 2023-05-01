ca76240233f0f72c074dbe69c62ffe151a0fe833
Vicent29 <esteve.ferre.vicent@gmail.com>
2023-05-02 00:08:37 +0200

    Electron resize window

--- Frontend/electron/main.js
+++ Frontend/electron/main.js
@@ -1,28 +1,51 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const electron = require('electron')
+const {Menu} = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
 
-const path = require('path');
-const isDev = require('electron-is-dev');
+const path = require('path')
+let mainWindow
+let isMobile = false
 
-let mainWindow;
+
+const template = [{
+    label: 'Admin', submenu: [
+        {
+            label: 'Change View', click: async () => {
+                isMobile = !isMobile
+                if (isMobile) {
+                    mainWindow.setSize(428, 926, true)
+                } else {
+                    mainWindow.setSize(900, 680, true)
+                }
+            }
+        }
+    ]
+}]
+
+const menuMobile = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menuMobile)
 
 function createWindow() {
-    mainWindow = new BrowserWindow({width: 900, height: 680});
-    mainWindow.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
-    mainWindow.on('closed', () => mainWindow = null);
+    mainWindow = new BrowserWindow({width: 900, height: 680})
+    mainWindow.webContents.toggleDevTools()
+    mainWindow.loadURL(`file://${path.join(__dirname, '../build/index.html')}`)
+    mainWindow.on('closed', () => mainWindow = null)
 }
 
-app.on('ready', createWindow);
+
+app.on('ready', () => {
+    createWindow()
+})
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        app.quit();
+        app.quit()
     }
-});
+})
 
 app.on('activate', () => {
     if (mainWindow === null) {
-        createWindow();
+        createWindow()
     }
-});
+})
